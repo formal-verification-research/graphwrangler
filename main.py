5b968988954932bcef5a24b4ccf2565f565aeaba
Bennett DenBleyker <bennett.denbleyker@usu.edu>
2021-06-03 18:15:57 -0600

    New parameter to negative the logits

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from load import load_graph
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        sys.stderr.write("Usage: python " + sys.argv[0] + " <graph.pb> <out_graph.pb>\n")
+        sys.stderr.write("Usage: python " + sys.argv[0] + " <graph.pb> <out_graph.pb> [negative : bool]\n")
     else:
         sys.stderr.write("Loading " + sys.argv[1] + "\n")
         graph = load_graph(sys.argv[1])
@@ -22,6 +22,8 @@ if __name__ == '__main__':
         
         with graph.as_default():
             logits = graph.get_tensor_by_name(nodes["logits"].name + ":0")
+            if len(sys.argv) > 3 and sys.argv[3].lower() in ["true", "t", "1"]:
+                logits = tf.math.negative(logits, name="negative")
             x = graph.get_tensor_by_name(nodes["input"].name + ":0")
             y = tf.compat.v1.placeholder(tf.float32, shape=(None, 10), name='y_label')
             loss = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=logits, name='loss_fun')
